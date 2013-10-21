2836f1712d4eacd42e08e16b126c9d25f7f9a2eb
Jeremy Bell <grcodemonkey@live.com>
2013-10-21 01:31:53 -0400

    Common Solution Info file, updated Nuspec project and icon references

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("grcodemonkey")]
+
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("2013.10.55.845")]
+[assembly: AssemblyFileVersion("2013.10.55.845")]
